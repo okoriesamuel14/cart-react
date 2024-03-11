66a020264562c23549adcf7aaecbafa6e450d41b
Sammy <okoriesamuel14@yahoo.com>
2024-03-11 07:51:48 -0700

    cart react

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+
+<header>
+    <nav>
+        <div id="nav1">Wondering when your final order is been ship? View our product schedule date.</div>
+        <div id="nav2">
+            <div id="nav2a">
+    <p>All shoes</p>
+    <p>Home try on</p>
+    <p>Sizes and colors</p>
+            </div>
+            <div id="nav2b">
+                <p>Sammy's cart</p>
+            </div>
+            <div id="nav2c">
+                <p>Our story</p>
+                <p>Search</p>
+                <p>My account</p>
+                <img src="res/fff.png" alt=""/>
+            </div>
+        </div>
+    </nav>
+</header>
+
+</div>
+    </>
+  )
+}
+
+export default App
